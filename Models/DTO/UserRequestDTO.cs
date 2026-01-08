1c7b113880553a752fff32625003e55297ccf174
Mardson581 <mardsongames@gmail.com>
2026-01-08 17:55:28 +0000

    Working on UserRepository

--- Models/DTO/UserRequestDTO.cs
+++ Models/DTO/UserRequestDTO.cs
@@ -4,12 +4,12 @@ namespace AspProfile.Models.DTO;
 
 public class UserRequestDTO
 {
-    [Required(ErrorMessage="O primeiro nome é obrigatório")]
+    public string Id { get; set; }
+    
     [MinLength(3, ErrorMessage="O nome deve ter no mínimo 3 caracteres")]
     [MaxLength(50, ErrorMessage="O nome deve ter no máximo 50 caracteres")]
     public string FirstName { get; set; }
 
-    [Required(ErrorMessage="O sobrenome é obrigatório")]
     [MinLength(5, ErrorMessage="O sobrenome deve ter no mínimo 5 caracteres")]
     [MaxLength(100, ErrorMessage="O sobrenome deve ter no máximo 100 caracteres")]
     public string LastName { get; set; }
@@ -18,7 +18,6 @@ public class UserRequestDTO
     [EmailAddress(ErrorMessage="O Email é inválido")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage="A descrição é obrigatória")]
     [MinLength(10, ErrorMessage="A descrição deve ter no mínimo 10 caracteres")]
     [MaxLength(200, ErrorMessage="A descrição deve ter no máximo 200 caracteres")]
     public string Description { get; set; }
